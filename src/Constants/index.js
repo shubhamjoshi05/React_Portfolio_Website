33d4e113e2b0fe1098eda0650588ff1e5fde83f4
shubhamjoshi05 <122545103+shubhamjoshi05@users.noreply.github.com>
2023-09-30 21:43:05 +0530

    Add files via upload

---
+++ src/Constants/index.js
@@ -0,0 +1,230 @@
+import { aflacLogo, android, angular, codeclause, cognizantLogo,
+     css,
+     facebook, firebase, git, github, hibernate, html, instagram, ionic, java, linkdin, mysql,
+      nodejs,
+      novitech,
+      oasis,
+      python,
+      react, spark, spring, sql, typescript, wellsLogo, wiuLogo, youtube } from "../assets"
+
+export const AppText={
+    hello:'Hello,',
+    Iam:'I am ',
+    shubhamJoshi:'Shubham Joshi',
+    aboutMeDescripion:'I am Fullstack Software Developer, Expert in Front-end and Back-end Development, UI/UX design and i have skills in Java, Python, Sql, Servlets, JSP, JDBC, Hibernate, Spring Framework, HTML, CSS, JavaScript, GIT, GitHub, React.js.',
+    Skills:'Skills & ',
+    Experties:'Experties',
+    UIUXDesigner:'FullStack Software Developer',
+    BackendDeveloper:'Backend Dev',
+    FrontedDeveloper:'Frontend Dev',
+    Portfolio:'Portfolio',
+    Creative:'My Creative ',
+    CompaniesI:'Companies I ',
+    WorkedFor:'Worked For',
+    Contact:'Contact ',
+    Us:'Us',
+    copywriteText:'Copyright by Shubham Joshi @2023'
+
+}
+
+export const aboutSection=[
+    {
+        id:1,
+        image:'https://img.freepik.com/premium-vector/ui-ux-programmer-flat-design-vector-illustration-business-information-team-sharing-ideas-with-designer-coding-interface-software-app-development_2175-1809.jpg?w=2000',
+        title:'UI/UX Designer',
+        desc:'Expert in UI/UX designing'
+    },
+    {
+        id:2,
+        image:'https://media.istockphoto.com/id/1304570729/vector/front-end-development-concept-vector-flat-graphic-design-illustration.jpg?s=612x612&w=0&k=20&c=0lpu0j-4FbuoA7xpGzt9apKZeI5F9KlNNe0qRYxjUYs=',
+        title:'Frontend Developer',
+        desc:'Expert in designing creative frontend designs using Technologies like html, css, react.js'
+    },
+    {
+        id:3,
+        image:'https://img.freepik.com/premium-vector/back-end-developer-working-laptop_701961-1383.jpg?w=2000',
+        title:'Back-end Developer',
+        desc:'Expert in back-end Technologies like Hibernate, Spring framework, Java, Servlets, SQL Database'
+    }
+]
+
+export const skillsList=[
+    {
+        id:1,
+        icon:angular
+    },
+    {
+        id:1,
+        icon:react
+    },
+    {
+        id:1,
+        icon:typescript
+    },
+    {
+        id:1,
+        icon:firebase
+    },
+    {
+        id:1,
+        icon:ionic
+    },
+    {
+        id:1,
+        icon:mysql
+    },
+    {
+        id:1,
+        icon:react
+    },
+    {
+        id:1,
+        icon:typescript
+    },
+    {
+        id:1,
+        icon:firebase
+    },
+    {
+        id:1,
+        icon:nodejs
+    },
+     {
+        id:1,
+        icon:java
+    },
+    {
+        id:1,
+        icon:sql
+    },
+    {
+        id:1,
+        icon:hibernate
+    },
+    {
+        id:1,
+        icon:spring
+    },
+    {
+        id:1,
+        icon:html
+    },
+    {
+        id:1,
+        icon:css
+    },
+    {
+        id:1,
+        icon:python
+    },
+    {
+        id:1,
+        icon:git
+    },
+    {
+        id:1,
+        icon:github
+    },
+]
+
+export const workDetail=[
+    { 
+        id:1,
+        year:'June 2023',
+        compnayName:'NoviTech R&D Pvt Ltd ',
+        position:'Fullstack Development Intern'
+    },
+    { 
+        id:1,
+        year:'July 2023',
+        compnayName:'CodeClause',
+        position:'Java Development Intern'
+    },
+    { 
+        id:1,
+        year:'August 2023',
+        compnayName:'Oasis Infobyte',
+        position:'Java Programming Intern'
+    },
+    { 
+        id:1,
+        year:'Sept 2023',
+        compnayName:'The Spark Foundation',
+        position:'Web Development Intern'
+    },
+]
+
+export const portfolio=[
+    {
+        id:1,
+        title:'Food Delivery UI/UX',
+        type:'ui/ux',
+        desc:"App Build using React native and compaitable for Android and IOS",
+        imageUrl:"https://cdn.dribbble.com/users/4189231/screenshots/17343305/media/202173138f9504c2eacd16e70698312c.png?compress=1&resize=400x300"
+    },
+    {
+        id:4,
+        title:'Funny Website',
+        type:'website',
+        desc:"App Build using React native and compaitable for Android and IOS",
+        imageUrl:"https://miro.medium.com/max/1400/0*GWvV5kcuVATUfs2r.png"
+    },
+    {
+        id:2,
+        title:'Business Listing App',
+        type:'mobile',
+        desc:"App Build using React native and compaitable for Android and IOS",
+        imageUrl:"https://www.radiustheme.com/wp-content/uploads/2022/05/Directory-app.png"
+    },
+    {
+        id:3,
+        title:'Food Delivery UI/UX',
+        type:'ui/ux',
+        desc:"App Build using React native and compaitable for Android and IOS",
+        imageUrl:"https://cdn.dribbble.com/users/2892962/screenshots/14273462/nlytical_mockup_4x.png"
+    },
+
+    
+]
+
+export const CompanyImage=[
+    {
+        id:1,
+        imageUrl:novitech
+    },
+    {
+        id:1,
+        imageUrl:codeclause
+    },
+    {
+        id:1,
+        imageUrl:oasis
+    },
+    {
+        id:1,
+        imageUrl:spark
+    }
+]
+
+export const socialNetwork=[
+    {
+        id:1,
+        logo:instagram,
+        url:'https://www.instagram.com/shubhamjoshi_530/#'
+    },
+    {
+        id:2,
+        logo:youtube,
+        url:'https://www.youtube.com',
+    },
+    {
+        id:3,
+        logo:linkdin,
+        url:'- https://www.linkedin.com/in/shubham-joshi-23b339197'
+    },
+    {
+        id:4,
+        logo:facebook,
+        url:'https://www.facebook.com/Shubham1137/'
+    },
+]
---
+++ src/assets/index.js
@@ -0,0 +1,74 @@
+import logo from './logo.png'
+import homeImage from './homeImage.png'
+import book from './book.png'
+import laptop from './laptop.png'
+import skill from './skill.png'
+
+import angular from './angular.png'
+import ionic from './ionic.png'
+import firebase from './firebase.png'
+import typescript from './typescript.png'
+import mysql from './mysql.png'
+import nodejs from './nodejs.png'
+
+import gitlab from './gitlab.jpg'
+import android from './android.png'
+import react from './react.png'
+import wave from './wave.svg'
+import robo from './robo.png'
+import cognizantLogo from './cognizantLogo.png'
+import wellsLogo from './wells.png'
+import aflacLogo from './aflac.png'
+import wiuLogo from './wiu.png'
+import contactusbg from './contactusbg.png'
+import phone from './phone.png'
+import youtube from './youtube.png'
+import instagram from './Instagram.png'
+import facebook from './Facebook.png'
+import linkdin from './LinkedIn.png'
+import download from './download.jpg'
+import extra from './extra.jpg'
+import codeclause from './codeclause.png'
+import novitech from './novitech.jpg'
+import oasis from './oasis.png'
+import spark from './spark.png'
+import java from './java.jpg'
+import sql from './sql.jpg'
+import hibernate from './hibernate.jpg'
+import spring from './spring.jpg'
+import html from './html.jpg'
+import css from './css.jpg'
+import python from './python.jpg'
+import git from './git.jpg'
+import github from './github.jpg'
+
+
+
+
+
+
+
+export {
+    logo,cognizantLogo,wellsLogo,nodejs,contactusbg,phone,youtube,instagram,facebook,linkdin,
+    homeImage,aflacLogo,wiuLogo,
+    book,
+    laptop,
+    skill,
+    angular,
+    typescript,
+    android,
+    gitlab,
+    ionic,
+    firebase,
+    mysql,
+    react,
+    wave,
+    robo,
+    download,
+    extra,
+    codeclause,
+    novitech,
+    oasis,
+    spark,
+    java,sql,hibernate,spring,html,css,python,git,github
+}
